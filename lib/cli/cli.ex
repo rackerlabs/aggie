0b36f3a56c2b2627dca057a60eec0e2cefcdc949
Matt Darby <matt.darby@rackspace.com>
2017-03-06 09:50:32 -0500

    Added version number

--- lib/cli/cli.ex
+++ lib/cli/cli.ex
@@ -1,11 +1,20 @@
 defmodule Aggie.CLI do
 
+  @version 0.1
+
   def main(args) do
-    {opts,_,_} = OptionParser.parse(args, switches: [syslog: :boolean, tenant_id: :string])
+    {opts,_,_} = OptionParser.parse(args, switches:
+      [
+        syslog: :boolean,
+        tenant_id: :string,
+        version: :boolean
+      ]
+    )
 
     cond do
       valid_syslog_setup?(opts) -> Aggie.start_syslog_server(opts)
       valid_elk_setup?(opts) -> Aggie.Elk.ship_latest_logs!
+      asking_for_version?(opts) -> print_version()
       true -> print_usage()
     end
   end
@@ -18,8 +27,16 @@ defmodule Aggie.CLI do
     Keyword.has_key?(opts, :tenant_id)
   end
 
+  defp asking_for_version?(opts) do
+    Keyword.has_key?(opts, :version)
+  end
+
   defp print_usage do
     IO.puts "Error: Requires --tenant-id"
+  end
+
+  defp print_version do
+    IO.puts "Aggie v#{@version}"
   end
 
 end
