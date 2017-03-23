77f4e79f3dbfde402784daa5549953900f90580f
Matt Darby <matt.darby@rackspace.com>
2017-03-23 13:43:57 -0400

    Getting the payload

--- lib/elk/action.ex
+++ lib/elk/action.ex
@@ -1,4 +1,22 @@
+require IEx
+
 defmodule Aggie.Elk.Action do
+
+  # MISSING:
+  # Duration
+  # Assigned Host
+  # Error message
+  # Flavor and specs and tags
+  # Network Ports Information (see below)
+  # Attached volumes
+
+  @uuid_regex        ~r/, id:\s([a-z|0-9|-]*), size:/
+  @project_id_regex  ~r/projects\/([a-z|0-9]*)/
+  @domain_id_regex   ~r/domain_id\": \"(\w+)\"/
+  @request_id_regex  ~r/\[(req[a-z|0-9|-]*)/
+  @image_name_regex  ~r/image_name:\s(.*), image_id:/
+  @image_id_regex    ~r/image_id:\s(.*), container/
+
 
   @doc """
   Aggregate OpenStack logs into cohesive actions via request ID
@@ -7,6 +25,27 @@ defmodule Aggie.Elk.Action do
     Enum.reduce get_latest_request_ids(), [], fn(id, acc) ->
       acc ++ [get_info_about_request(id)]
     end
+  end
+
+  def parse_action(action) do
+    string     = action |> Enum.join(" ")
+    uuid       = Regex.run(@uuid_regex, string) |> List.last
+    project_id = Regex.run(@project_id_regex, string) |> List.last
+    domain_id  = Regex.run(@domain_id_regex, string) |> List.last
+    request_id = Regex.run(@request_id_regex, string) |> List.last
+    image_name = Regex.run(@image_name_regex, string) |> List.last
+    image_id   = Regex.run(@image_id_regex, string) |> List.last
+
+    IEx.pry
+
+    %{
+      uuid: uuid,
+      project_id: project_id,
+      domain_id: domain_id,
+      request_id: request_id,
+      image_name: image_name,
+      image_id: image_id
+    }
   end
 
 
