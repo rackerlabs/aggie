7ee40da591214c7f409611c7c8fa537fe170fb3f
Matt Darby <matt.darby@rackspace.com>
2017-03-31 16:39:57 -0400

    Cleaned up tests

--- test/aggie_test.exs
+++ test/aggie_test.exs
@@ -2,38 +2,8 @@ require IEx
 
 defmodule AggieTest do
   use ExUnit.Case
-  doctest Aggie
 
   test "latest actions" do
-    assert Aggie.Elk.Action.latest_actions |> Enum.count > 1
+    assert Aggie.Elk.latest_logs() |> Enum.count > 1
   end
-
-  test "we have data" do
-    regex = ~r/darby/
-
-    action = Enum.find(Aggie.Elk.latest_actions, fn(action) ->
-      Enum.any?(action, fn(str) -> Regex.match?(regex, str) end)
-    end)
-
-    assert action
-  end
-
-  test "payload" do
-    {:ok, body} = File.read("test/sample_action.txt")
-    {:ok, json} = Poison.decode(body)
-    out = Aggie.Elk.Action.parse_action(json)
-    assert out.uuid
-    assert out.project_id
-    assert out.domain_id
-    assert out.request_id
-    assert out.image_name
-    assert out.image_id
-  end
-
-  test "valuable info" do
-    Enum.each(Aggie.Elk.Action.latest_actions(), fn(a) ->
-      IO.inspect a
-    end)
-  end
-
 end
