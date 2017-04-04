a0cfe4e15f059f3f7ae0833b86f08f4c747d234c
Matt Darby <matt.darby@rackspace.com>
2017-04-04 15:14:24 -0400

    Working, but only with shell scripts :/

--- rel/config.exs
+++ rel/config.exs
@@ -6,22 +6,18 @@ use Mix.Releases.Config,
     default_release: :default,
     default_environment: Mix.env()
 
-environment :default do
-  set commands: [
-    ship: "rel/commands/ship.sh"
-  ]
-end
-
 environment :dev do
   set dev_mode: true
   set include_erts: false
   set cookie: :"NiwyQDR~tMSJ~wDuibRKZIG9]W>eVQr`f1!I]x.Zq:xIKw:1tXy,%8:w/;>Wq2CW"
+  set commands: [ "ship.sh": "rel/commands/ship.sh" ]
 end
 
 environment :prod do
   set include_src: false
   set include_erts: "ubuntu_erts"
   set cookie: :"Ltz/hf`Q~usQ??>h8&yts&=yl6LON8]{iGa4p6t!R:/![}A$4bdKOzRd.B^R6:X%"
+  set commands: [ "ship.sh": "rel/commands/ship.sh" ]
 end
 
 release :aggie do
